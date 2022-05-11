c38c13aea3cae68ff4925bf336ed5e048d0e155d
Nurdin <nuridin02kg02@gmail.com>
2022-05-11 16:40:42 +0600

    Homework 14 last version

--- src/com/company/Singer.java
+++ src/com/company/Singer.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Singer extends Person{
+
     private String bandName;
     public Singer(String name, String desingnation,String bandName) {
         super(name, desingnation);
